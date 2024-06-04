98a8b5a1f8766030dd12f47ccbbf3cb5a3c53385
marcioeric <marcio.eric.nicolau@gmail.com>
2024-06-04 14:58:56 -0300

    First Commit

---
+++ src/client/java/net/revivalbytes/Industrial_revivalClient.java
@@ -0,0 +1,10 @@
+package net.revivalbytes;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Industrial_revivalClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
